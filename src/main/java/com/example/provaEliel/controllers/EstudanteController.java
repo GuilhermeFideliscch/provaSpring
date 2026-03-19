f896bda8f4fe73be718d315744995df1e3519031
guilherme Chiquito <guilherme.fidelis2570@gmail.com>
2026-03-19 20:21:21 -0300

    commit test

--- src/main/java/com/example/provaEliel/controllers/EstudanteController.java
+++ src/main/java/com/example/provaEliel/controllers/EstudanteController.java
@@ -17,7 +17,7 @@ public class EstudanteController {
 
     @Autowired
     private EstudanteService estudanteService;
-    //teste
+    //teste aa
     @GetMapping
     public ResponseEntity<List<EstudanteModel>> findAll() {
         List<EstudanteModel> estudantes = estudanteService.findALL();
